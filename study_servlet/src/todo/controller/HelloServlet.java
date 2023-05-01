8a0ef8085a8f54bb36ce1daa55a824a8baec6552
320377 <320377@NH201904-D005.ad.narikoma.com>
2023-05-01 13:01:04 +0900

    a

--- study_servlet/src/todo/controller/HelloServlet.java
+++ study_servlet/src/todo/controller/HelloServlet.java
@@ -1,6 +1,8 @@
 package todo.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HelloServlet")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,16 +28,29 @@ public class HelloServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String name = (String) request.getAttribute("userName");
+
+	    if (name == null || "".equals(name)) {
+	        request.setAttribute("userName", "Guest");
+	    }
+
+	    String view = "/WEB-INF/view/index.jsp";
+	    RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+	    dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// 文字コードの指定
+	    request.setCharacterEncoding("utf-8");
+	    // formから値を取得
+	    String name = request.getParameter("name");
+
+	    request.setAttribute("userName", name);
+
+	    doGet(request, response);
 	}
 
 }
